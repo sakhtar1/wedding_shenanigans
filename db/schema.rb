2e98f5c213194b00e733613fa30b8b6b28869909
Sharmina Akhtar <sakht318@gmail.com>
2018-04-03 18:02:56 +0000

    db list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403155005) do
+ActiveRecord::Schema.define(version: 20180403174743) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.string "item"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
