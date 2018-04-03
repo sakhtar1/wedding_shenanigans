a12f138e0221d93788b04e2a7a8e31fba4a707fd
Sharmina Akhtar <sakht318@gmail.com>
2018-04-03 13:32:20 +0000

    adding app

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+  get "/signup" do
+    if !logged_in?
+		    erb :"users/create_user"
+    else
+      redirect '/tweets'
+    end
+	end
+
+  post "/signup" do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+        redirect "/signup"
+    else
+      @user = User.create(:username => params[:username], :email => params[:email],:password => params[:password])
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    end
+  end
+
+  get "/login" do
+    if !logged_in?
+		    erb :"users/login"
+    else
+      redirect '/tweets'
+    end
+	end
+
+  post "/login" do
+		 @user = User.find_by(:username => params[:username])
+		 if @user && @user.authenticate(params[:password])
+			 session[:user_id] = @user.id
+      redirect "/tweets"
+     else
+      redirect "/signup"
+		end
+	end
+
+  get "/logout" do
+    if logged_in?
+  		session.clear
+  		redirect "/users/login"
+    else
+      redirect "/"
+    end
+	end
+
+end
