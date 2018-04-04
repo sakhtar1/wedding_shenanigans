7c7aacd02b7d0bcdd536d18a5f98c12bea1b933d
Sharmina Akhtar <sakht318@gmail.com>
2018-04-04 19:13:26 +0000

    list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,9 @@
 class ListsController < ApplicationController
 
   get '/lists' do
-    if logged_in?
+      not_logged_in
       @lists = List.all
       erb :'lists/lists'
-    else
-      not_logged_in
-    end
   end
 
   get '/lists/new' do
@@ -14,11 +11,12 @@ class ListsController < ApplicationController
     redirect '/lists/new_list'
   end
 
-  get '/tweets/:id' do
+  get '/lists/:id' do
     not_logged_in
-    @tweet = Tweet.find_by_id(params[:id])
+    @list = List.find_by_id(params[:id])
     erb :'lists/show_list'
-
   end
+
+
 
 end
--- config.ru
+++ config.ru
@@ -1,12 +1,14 @@
+
 require './config/environment'
+
 
 if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
-use Rack::Static, :root => 'public', :urls => ['/images', '/stylesheets']
-
 use Rack::MethodOverride
-use UsersController
-use ListsController
+#use Rack::Flash
+
 run ApplicationController
+use ListsController
+use UsersController
